eab8f9672f662b667016e55e70ed1a0f0802e75e
Branch Vincent <vincentbranch23@gmail.com>
2026-04-23 16:40:15 +0700

    Allow redacted database status checks

--- api/db-status/index.php
+++ api/db-status/index.php
@@ -4,7 +4,19 @@ declare(strict_types=1);
 require dirname(__DIR__, 2) . '/partner-auth.php';
 require_once dirname(__DIR__, 2) . '/partner-data-bootstrap.php';
 
-jg_partner_require_auth_json();
-
 header('Content-Type: application/json; charset=utf-8');
-echo json_encode(jg_partner_data_status(), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+
+$status = jg_partner_data_status();
+
+if (!jg_partner_is_authenticated()) {
+    unset($status['database_name'], $status['database_user']);
+    foreach ($status['config_files'] as &$configFile) {
+        unset($configFile['path']);
+    }
+    unset($configFile);
+    $status['authenticated'] = false;
+} else {
+    $status['authenticated'] = true;
+}
+
+echo json_encode($status, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
